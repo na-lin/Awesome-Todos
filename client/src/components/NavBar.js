cb38fdb9c3e44a9961e96c4dea63f699c4cba4fb
Na Lin <lna483018@gmail.com>
2023-01-19 12:09:24 -0500

    style: add navbar

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+export default function NavBar() {
+  return (
+    <Box sx={{ display: "flex", bgcolor: "#06b6d4", p: 4 }}>
+      <Typography>Todos</Typography>
+    </Box>
+  );
+}
