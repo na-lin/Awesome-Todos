256a3e98ba0ea05dd08da8459d2894ebe6891038
Na Lin <lna483018@gmail.com>
2023-01-20 21:25:11 -0500

    connect: connect server to database

--- server/db/database.js
+++ server/db/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 // replace boilermaker with your database name
-const db_url = "postgres://localhost:5432/boilermaker";
+const db_url = "postgres://localhost:5432/todo";
 
 const db = new Sequelize(db_url, {
   logging: false,
