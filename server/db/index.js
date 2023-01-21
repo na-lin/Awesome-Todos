b524a4ef4fcdf659b9cf796d0d88754a62cd9f67
Na Lin <lna483018@gmail.com>
2023-01-20 22:30:44 -0500

    add: set relationship between task and date

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,7 @@ const db = require("./database");
 const User = require("./models/userModel");
 const Task = require("./models/taskModel");
 const Detail = require("./models/detailModel");
+const Date = require("./models/dateModel");
 
 // Here to define associate between models
 User.hasMany(Task);
@@ -11,9 +12,13 @@ Task.belongsTo(User);
 Task.hasOne(Detail);
 Detail.belongsTo(Task);
 
+Task.hasOne(Date);
+Date.belongsTo(Task);
+
 module.exports = {
   db,
   User,
   Task,
   Detail,
+  Date,
 };
