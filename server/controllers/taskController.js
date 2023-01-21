2f5ce13a4310d850d859cbac5451a22f6741eaab
Na Lin <lna483018@gmail.com>
2023-01-20 23:38:30 -0500

    feat: user can create a task

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -2,4 +2,27 @@ const asyncHandler = require("express-async-handler");
 const AppError = require("../utils/appError");
 const { Task, Date, Detail } = require("../db");
 
-module.exports = {};
+// @desc: Create a task
+// @route: POST /api/task
+// @access: Private
+const createNewTask = asyncHandler(async (req, res, next) => {
+  const { title, state, priority, date } = req.body;
+  const task = await Task.create({ title, state, priority });
+  let taskDate;
+  if (date) {
+    taskDate = await Date.create({ date });
+    taskDate.setTask(task);
+  }
+  task.setUser(req.user);
+  task.userId = undefined;
+
+  res.status(201).json({
+    status: "success",
+    task: {
+      ...task.toJSON(),
+      date: taskDate?.date,
+    },
+  });
+});
+
+module.exports = { createNewTask };
--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../controllers/authController");
+const { createNewTask } = require("../controllers/taskController");
+
+// route start with /api/task
+router.route("/").post(protect, createNewTask);
 
 module.exports = router;
