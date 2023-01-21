7558fc3648088df39f9d7a1263787a07dc55a050
Na Lin <lna483018@gmail.com>
2023-01-20 21:29:40 -0500

    feat: define user model

---
+++ server/db/data/users.js
@@ -0,0 +1,44 @@
+module.exports = [
+  {
+    name: "demo",
+    email: "demo@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "John",
+    email: "john@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Anne",
+    email: "anne@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Tessa",
+    email: "tessa@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Joy",
+    email: "joy@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "Cindy",
+    email: "cindy@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+  {
+    name: "poppy",
+    email: "poppy@example.com",
+    password: "123456",
+    passwordConfirm: "123456",
+  },
+];
