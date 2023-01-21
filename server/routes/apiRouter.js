a2996801988fce9201effd5ba19db942079a2f0c
Na Lin <lna483018@gmail.com>
2023-01-20 23:00:29 -0500

    add: create route for teask

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -10,5 +10,6 @@ router.use((req, res, next) => {
 // Sub-router
 router.use("/template", require("./templateRouter"));
 router.use("/auth", require("./authRouter"));
+router.use("/task", require("./taskRouter"));
 
 module.exports = router;
