37c3bd467905d3b621bffe6771a6430251464384
Na Lin <lna483018@gmail.com>
2023-01-20 22:25:23 -0500

    add: set relationship between user and tasks

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,14 @@
 const db = require("./database");
 
 const User = require("./models/userModel");
+const Task = require("./models/taskModel");
 
 // Here to define associate between models
+User.hasMany(Task);
+Task.belongsTo(User);
 
 module.exports = {
   db,
   User,
+  Task,
 };
