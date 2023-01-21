6ee6282c06e812ac501356b767a6b80cb56ae0f5
Na Lin <lna483018@gmail.com>
2023-01-20 22:28:19 -0500

    add: set association between task and detail

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,18 @@ const db = require("./database");
 
 const User = require("./models/userModel");
 const Task = require("./models/taskModel");
+const Detail = require("./models/detailModel");
 
 // Here to define associate between models
 User.hasMany(Task);
 Task.belongsTo(User);
 
+Task.hasOne(Detail);
+Detail.belongsTo(Task);
+
 module.exports = {
   db,
   User,
   Task,
+  Detail,
 };
