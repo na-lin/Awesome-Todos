1a9f709287585dc12046a6cca2c20264080c5daa
Na Lin <lna483018@gmail.com>
2023-01-26 08:44:54 -0500

    add routes to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
+// Router
+import { Routes, Route } from "react-router-dom";
 // screen
 import LandingScreen from "./screens/LandingScreen";
 
 export default function App() {
   return (
     <div>
-      <LandingScreen />
+      <Routes>
+        <Route path="/" element={<LandingScreen />} />
+      </Routes>
     </div>
   );
 }
