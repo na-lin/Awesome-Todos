31d48ce9ae9daaae54a543f92a0dfd05d6295b3d
Na Lin <lna483018@gmail.com>
2023-01-26 08:52:54 -0500

    add route to render login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 // screen
 import LandingScreen from "./screens/LandingScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
       </Routes>
     </div>
   );
