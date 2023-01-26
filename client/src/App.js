b0601e2bebe8fbe99d4abb8ef35531bd3db8003d
Na Lin <lna483018@gmail.com>
2023-01-26 02:17:35 -0500

    clear App.js to focus on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,12 @@
-import NavBar from "./components/NavBar";
-import SideBar from "./components/SideBar";
-import TaskScreen from "./screens/TaskScreen";
-import TaskDetail from "./screens/TaskDetail";
-import { Container, Box } from "@mui/material";
-function App() {
+import React from "react";
+
+// screen
+import LandingScreen from "./screens/LandingScreen";
+
+export default function App() {
   return (
-    <Container maxWidth="lg">
-      <NavBar />
-      <Box sx={{ display: "flex" }}>
-        <SideBar />
-        <TaskScreen sx={{ flexGrow: 1, maxWidth: "50%" }} />
-        <TaskDetail sx={{ flexGrow: 1, maxWidth: "50%" }} />
-      </Box>
-    </Container>
+    <div>
+      <LandingScreen />
+    </div>
   );
 }
-
-export default App;
