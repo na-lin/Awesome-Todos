ab5728ca03dd3f378884c7eebe2a873bcbae8809
Na Lin <lna483018@gmail.com>
2023-01-25 21:09:34 -0500

    test routes for RTK query

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,36 @@ import SideBar from "./components/SideBar";
 import TaskScreen from "./screens/TaskScreen";
 import TaskDetail from "./screens/TaskDetail";
 import { Container, Box } from "@mui/material";
+import { Routes, Route, Link } from "react-router-dom";
+
+// sreen
+import LoginScreen from "./screens/LoginScreen";
 function App() {
+  console.log("app");
   return (
     <Container maxWidth="lg">
       <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              Page 1<Link to={"/login"}>to login</Link>
+            </>
+          }
+        />
+        <Route
+          path="/t2"
+          element={
+            <>
+              Page 2<Link to={"/"}>to test 1</Link>
+            </>
+          }
+        />
+
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+
       <Box sx={{ display: "flex" }}>
         <SideBar />
         <TaskScreen sx={{ flexGrow: 1, maxWidth: "50%" }} />
