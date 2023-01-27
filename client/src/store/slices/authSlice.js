886ee9cf06f05ba6702212c59357e96aeaf439c6
Na Lin <lna483018@gmail.com>
2023-01-26 21:47:47 -0500

    create auth slice

---
+++ client/src/store/slices/authSlice.js
@@ -0,0 +1,71 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const userLogin = createAsyncThunk(
+  "user/login",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      // configure header's Content-Type as JSON
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/auth/login",
+        { email, password },
+        config
+      );
+
+      // store user's token in local storage
+      localStorage.setItem("userToken", data.userToken);
+
+      return data;
+    } catch (error) {
+      // return custom error message from API if any
+      if (error.response && error.response.data.message) {
+        return rejectWithValue(error.response.data.message);
+      } else {
+        return rejectWithValue(error.message);
+      }
+    }
+  }
+);
+
+// initialize userToken from local storage
+const userToken = localStorage.getItem("userToken")
+  ? localStorage.getItem("userToken")
+  : null;
+
+const initialState = {
+  loading: false,
+  userInfo: null,
+  userToken,
+  error: null,
+  success: false,
+};
+
+const authSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    // login user
+    [userLogin.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    [userLogin.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.userInfo = payload;
+      state.userToken = payload.userToken;
+    },
+    [userLogin.rejected]: (state, { payload }) => {
+      state.loading = false;
+      state.error = payload;
+    },
+  },
+});
+
+export default authSlice.reducers;
