25c28934013d956ee519d21132f4ced3b0fb529b
Na Lin <lna483018@gmail.com>
2023-01-26 23:37:48 -0500

    add layout to dashboard

---
+++ client/app/screens/Dashboard/DashboardScreen.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+// MUI
+import { Box, Typography, Tooltip, IconButton, Icon } from "@mui/material";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
+
+// todo: active color in current dashboard
+
+export default function DashboardScreen() {
+  return (
+    <Box>
+      <header>
+        <Box sx={{ display: "flex", alignItems: "center", px: 6, py: 2 }}>
+          <img src="./images/favicon.ico" alt="logo" />
+          <Typography
+            variant="h5"
+            sx={{ mx: 2, color: "#209CEE", fontWeight: "bold" }}
+          >
+            Awesome Todos
+          </Typography>
+        </Box>
+      </header>
+      <Box display={"flex"}>
+        <Box
+          display="flex"
+          flexDirection={"column"}
+          sx={{
+            px: 4,
+            py: 2,
+          }}
+        >
+          <Tooltip title="todo">
+            <IconButton sx={{ color: "#209CEE" }}>
+              <CheckBoxIcon fontSize="large" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="journal">
+            <IconButton>
+              <LocalLibraryIcon fontSize="large" />
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Box
+          sx={{
+            bgcolor: "#ecfeff",
+            width: 1,
+            height: "100vh",
+            border: 1,
+            borderColor: "#eee",
+          }}
+        >
+          Dashboard content
+        </Box>
+      </Box>
+    </Box>
+  );
+}
