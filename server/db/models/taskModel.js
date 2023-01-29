364e9c813f47710be406e9a476d615b561fe7065
Na Lin <lna483018@gmail.com>
2023-01-29 14:58:50 -0500

    clear backend code to MVP

--- server/db/models/taskModel.js
+++ server/db/models/taskModel.js
@@ -6,14 +6,6 @@ const Task = db.define("task", {
     type: Sequelize.STRING,
     allowNull: false,
   },
-  state: {
-    type: Sequelize.ENUM("todo", "completed", "won't do"),
-    defaultValue: "todo",
-  },
-  priority: {
-    type: Sequelize.ENUM("urgent", "high", "medium", "low"),
-    defaultValue: "low",
-  },
 });
 
 module.exports = Task;
--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getAllTasks,
-  getTask,
-  createNewTask,
-  updateDetail,
-  getTaskDetail,
-  deleteTask,
-  updateTask,
-} = require("../controllers/taskController");
+const {} = require("../controllers/taskController");
 
 // route start with /api/task
-router.route("/").get(getAllTasks).post(createNewTask);
-
-router.route("/:taskId/detail").put(updateDetail).get(getTaskDetail);
-
-router.route("/:taskId").get(getTask).delete(deleteTask).put(updateTask);
 
 module.exports = router;
