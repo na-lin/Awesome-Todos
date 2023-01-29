6147815f37fcfa28ff7774fe9e699568113cbe07
Na Lin <lna483018@gmail.com>
2023-01-29 14:54:14 -0500

    add user menu in navbar

--- client/app/screens/Dashboard/DashboardScreen.js
+++ client/app/screens/Dashboard/DashboardScreen.js
@@ -1,27 +1,83 @@
 import React from "react";
 
 // MUI
-import { Box, Typography, Tooltip, IconButton, Icon } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Tooltip,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import LocalLibraryIcon from "@mui/icons-material/LocalLibrary";
-
+import AccountCircle from "@mui/icons-material/AccountCircle";
 // todo: active color in current dashboard
 
 // Dashboard component
 import TodoDashBoardScreen from "./TodoDashBoardScreen";
 
 export default function DashboardScreen() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box>
       <header>
-        <Box sx={{ display: "flex", alignItems: "center", px: 6, py: 2 }}>
-          <img src="./images/favicon.ico" alt="logo" />
-          <Typography
-            variant="h5"
-            sx={{ mx: 2, color: "#209CEE", fontWeight: "bold" }}
-          >
-            Awesome Todos
-          </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            px: 6,
+            py: 2,
+            justifyContent: "space-between",
+          }}
+        >
+          <Box display={"flex"} alignItems="center">
+            <img src="./images/favicon.ico" alt="logo" />
+            <Typography
+              variant="h5"
+              sx={{ mx: 2, color: "#209CEE", fontWeight: "bold" }}
+            >
+              Awesome Todos
+            </Typography>
+          </Box>
+
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+            >
+              <AccountCircle color="#209cee" />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Log out</MenuItem>
+            </Menu>
+          </div>
         </Box>
       </header>
       <Box display={"flex"}>
